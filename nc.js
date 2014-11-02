2b69cdea8415433b136e51ebc7b6b25d877a7af3
Ates Goral <ates@magnetiq.com>
2014-11-02 02:27:16 -0500

    Tabs to spaces

--- nc.js
+++ nc.js
@@ -1,27 +1,27 @@
 var express = require('express'),
-	fs = require('fs-plus'),
-	path = require('path');
+    fs = require('fs-plus'),
+    path = require('path');
 
 var app = express();
 
 app.use('/', express.static(__dirname + '/static'));
 
 app.get('/api/file/ls', function (request, response) {
-	response.json(
-		fs.listSync(fs.getHomeDirectory())
-			.map(function (pathname) {
-	  			var ext = path.extname(pathname),
-	  				name = path.basename(pathname, ext),
-	  				stat = fs.statSync(pathname);
+    response.json(
+        fs.listSync(fs.getHomeDirectory())
+            .map(function (pathname) {
+                var ext = path.extname(pathname),
+                    name = path.basename(pathname, ext),
+                    stat = fs.statSync(pathname);
 
-				  return {
-				      name: name,
-				      ext: ext,
-				      size: stat.size,
-				      lastModified: stat.mtime
-				  };
-			})
-	);
+                  return {
+                      name: name,
+                      ext: ext,
+                      size: stat.size,
+                      lastModified: stat.mtime
+                  };
+            })
+    );
 });
 
 app.listen(6153, '127.0.0.1');
