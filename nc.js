b2bf1201947279b0033446f5659079ada3a227fa
Ates Goral <ates@magnetiq.com>
2014-11-04 12:43:14 -0500

    Better access error handling

--- nc.js
+++ nc.js
@@ -13,37 +13,39 @@ app.get('/api/file/ls', function (request, response) {
 
     try {
         files = fs.listSync(dirPath); // @todo use async
+
+        response.json({
+            dirPath: dirPath,
+            parentPath: parentPath !== dirPath ? parentPath : null,
+            files: files
+                .map(function (_path) {
+                    var ext = path.extname(_path),
+                        name = path.basename(_path, ext),
+                        stat = fs.statSync(_path);
+
+                    return {
+                        path: _path,
+                        filename: path.basename(_path),
+                        name: name, // @todo redundant
+                        ext: ext.substr(1), // @todo redundant
+                        size: stat.size,
+                        isDirectory: stat.isDirectory(),
+                        lastChanged: stat.ctime,
+                        lastAccessed: stat.atime,
+                        lastModified: stat.mtime
+                    };
+                })
+        });
     } catch (e) {
-        if (e.code === 'EACCES') {
+        switch (e.code) {
+        case 'EACCES':
+        case 'EPERM':
             response.status(403).end();
-        } else {
+            return;
+        default:
             throw e;
         }
-        return;
     }
-
-    response.json({
-        dirPath: dirPath,
-        parentPath: parentPath !== dirPath ? parentPath : null,
-        files: files
-            .map(function (_path) {
-                var ext = path.extname(_path),
-                    name = path.basename(_path, ext),
-                    stat = fs.statSync(_path);
-
-                return {
-                    path: _path,
-                    filename: path.basename(_path),
-                    name: name, // @todo redundant
-                    ext: ext.substr(1), // @todo redundant
-                    size: stat.size,
-                    isDirectory: stat.isDirectory(),
-                    lastChanged: stat.ctime,
-                    lastAccessed: stat.atime,
-                    lastModified: stat.mtime
-                };
-            })
-    });
 });
 
 app.listen(6153, '127.0.0.1');
