29e5113f647849d76e4d0c6683c043a5194ceb77
Ates Goral <ates@magnetiq.com>
2014-11-04 12:20:10 -0500

    Moved static sources into src

---
+++ static/src/main.js
@@ -0,0 +1,15 @@
+require.config({
+    paths: {
+        'text': '../bower_components/requirejs-text/text',
+        'less': '../bower_components/require-less/less',
+        'lessc': '../bower_components/require-less/lessc',
+        'normalize': '../bower_components/require-less/normalize',
+        'angular': '../bower_components/angular/angular.min',
+    },
+    shim: {
+        angular: {
+            exports: 'angular'
+        }
+    },
+    deps: [ 'bootstrap' ]
+});
